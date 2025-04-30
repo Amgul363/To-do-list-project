3fa7adff384e494f458debaf18e49d1f9e09e809
Angel Betty <angelbetty608@gmail.com>
2025-04-30 15:32:53 +0300

    Created a function

--- to-do.js
+++ to-do.js
@@ -3,29 +3,43 @@ const addTaskButton = document.getElementById('add-task');
 const taskList = document.getElementById('task-list');
 
 
-addTaskButton.addEventListener('click',()=>{
- const taskText =taskInput.value.trim();
-  if (taskText !== ''){
-    const li = document.createElement('li');
-    li.textContent = taskText
+function addTask(){
+  const taskText =taskInput.value.trim();
+    if (taskText !== ''){
+      const li = document.createElement('li');
+      li.textContent = taskText
+      
 
-//creatint a delete button
-   const deleteButton = document.createElement('button');
-   deleteButton.textContent = '❌'//delete button symbol
-   deleteButton.addEventListener('click',()=>{
-    li.remove();
-    //remove task when clicked
-   });
+  //creatint a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌'//delete button symbol
+    deleteButton.addEventListener('click',()=>{
+      li.remove();
+      //remove task when clicked
+    });
 
+    //append/add delete button to the list item
 
+    li.appendChild(deleteButton);
 
+    //append the new task li to the task list ul
 
+    taskList.appendChild(li);
 
+    //clear the input after adding task
+    taskInput.value = '';
+    }
+}
+addTaskButton.addEventListener('click', ()=>{
+  addTask();
+}
+  
+)
 
-
-  }
-
-
-
-
-})
+taskInput.addEventListener('keydown',(event)=>{
+if (event.key==='Enter'){
+  //prevent default
+  event.preventDefault();
+  addTask();
+}
+});
